f1aba4858c584f6ba5bc805c68f7de97cd343051
ledat2110 <letuandat2110@gmail.com>
2021-02-21 08:10:46 +0700

    cross-entropy for cartpole env

---
+++ chapter_04/cartpole.py
@@ -0,0 +1,111 @@
+import gym
+from collections import namedtuple
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+HIDDEN_SIZE = 128
+BATCH_SIZE = 16
+PERCENTILE = 70
+
+class Net (nn.Module):
+    def __init__ (self, obs_size: int, hidden_size: int, n_actions: int):
+        super (Net, self).__init__()
+        self.net = nn.Sequential(
+                nn.Linear(obs_size, hidden_size),
+                nn.ReLU(),
+                nn.Linear(hidden_size, n_actions)
+                )
+
+    def forward (self, x: torch.Tensor):
+        return self.net(x)
+
+Episode = namedtuple("Episode", ['reward', 'steps'])
+EpisodeStep = namedtuple("EpisodeStep", ['observation', 'action'])
+
+def iterate_batches (env: gym.Env, net: nn.Module, batch_size: int):
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs = env.reset()
+    sm = nn.Softmax(dim=1)
+
+    while True:
+        obs_v = torch.FloatTensor([obs])
+        act_probs_v = sm(net(obs_v))
+        act_probs = act_probs_v.data.numpy()[0]
+
+        action = np.random.choice(len(act_probs), p=act_probs)
+        next_obs, reward, done, _ = env.step(action)
+
+        episode_reward += reward
+        step = EpisodeStep(obs, action)
+        episode_steps.append(step)
+
+        if done:
+            e = Episode(episode_reward, episode_steps)
+            batch.append(e)
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs = env.reset()
+
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+
+        obs = next_obs
+
+def filter_batch (batch: Episode, percentile: float):
+    # get the reward of each episodes
+    rewards = list(map(lambda s: s.reward, batch))
+    # find the value for the percentile
+    reward_bound = np.percentile(rewards, percentile)
+    reward_mean = float(np.mean(rewards))
+
+    train_obs = []
+    train_act = []
+    for reward, steps in batch:
+        if reward < reward_bound:
+            continue
+        train_obs.extend(map(lambda step: step.observation, steps))
+        train_act.extend(map(lambda step: step.action, steps))
+
+    train_obs_v = torch.FloatTensor(train_obs)
+    train_act_v = torch.LongTensor(train_act)
+    return train_obs_v, train_act_v, reward_bound, reward_mean
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    obs_size = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    net = Net(obs_size, HIDDEN_SIZE, n_actions)
+    objective = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.01)
+    writer = SummaryWriter(comment='-cartpole')
+
+    solved_episode = 0
+
+    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+        optimizer.zero_grad()
+        action_scores_v = net(obs_v)
+        loss_v = objective(action_scores_v, acts_v)
+        loss_v.backward()
+        optimizer.step()
+
+        print("%d: loss=%.3f, reward_mean=%.1f, rw_bound=%.1f"%(iter_no, loss_v.item(), reward_m, reward_b))
+        writer.add_scalar("loss", loss_v.item(), iter_no)
+        writer.add_scalar("reward_bound", reward_b, iter_no)
+        writer.add_scalar("reward_mean", reward_m, iter_no)
+
+        if reward_m >= 199:
+            solved_episode += 1
+        if solved_episode >= 10:
+            print("solved")
+            break
+
+    writer.close()
